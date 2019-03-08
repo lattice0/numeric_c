424a59ac0ec942f00690747e47f094b5ed8c34da
Lucas Zanella <me@lucaszanella.com>
2019-03-08 09:46:52 -0300

    small changes

--- main.c
+++ main.c
@@ -6,9 +6,15 @@ int main()
     printf("Hello, World!\n");
     Matrix a;
     Matrix b;
-    float aa[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    matrix_create(&a, aa, 3, 3);
-    matrix_scalar_multiply(3, &a, &b);
+    float aa[3][3] = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+    matrix_create(&a, aa, 4, 3);
     matrix_print(&a);
+    matrix_print(&a);
+
+    //matrix_print(&a);
+    //matrix_scalar_multiply(3.0, &a);
+    matrix_scalar_multiply(3.0, &a, &b);
+    matrix_print(&a);
+    matrix_print(&b);
     return 0;
 }
--- matrix.c
+++ matrix.c
@@ -7,33 +7,28 @@ void matrix_create(Matrix* a, const float *array, int lines, int columns)
     a->numbers = (float *) malloc(lines*columns);
     a->lines = lines;
     a->columns = columns;
+    printf("creating matrix with %d lines and %d columns \n", lines, columns);
     for (int i=0 ;i<a->lines; i++)
-        for (int j=0; j<a->columns; j++)
+        for (int j=0; j<a->columns; j++) {
+            printf("*(a->numbers+ %d ) = %d;\n", j+i*columns, *(array+j+i*columns));
             *(a->numbers+j+i*columns) = *(array+j+i*columns);
+        }
 }
 
-int matrix_init(Matrix* a, int number_of_lines, ...) {
-    va_list list;
-    va_start(list, number_of_lines);
-    for(int count=1; count<=number_of_lines; count++)   {
-        // Get next argument in list
-        float * line = va_arg(list, float *);
-        printf('line ' + count);
-        printf(sizeof(line)/sizeof(line[0]));
-    }
-
-    // Clean the list
-    va_end(list);
+void matrix_init(Matrix* a, int lines, int columns)
+{   
+    a->numbers = (float *) malloc(lines*columns);
+    a->lines = lines;
+    a->columns = columns;
 }
 
 int matrix_scalar_multiply(float scalar, Matrix* a, Matrix* answer) {
-    printf("hello\n");
-    for (int i=0 ;i < a->lines; i++) {
+    matrix_init(answer, a->lines, a->columns);
+    for (int i=0 ;i < a->lines; i++)
         for (int j=0; j < a->columns; j++)
-            printf("%f   ", *(a->numbers+j+i*a->columns));
-        printf("\n");
-    }
-    printf("\n");
+            break;
+            //*(answer->numbers+j+i*answer->columns) = *(a->numbers+j+i*a->columns) * scalar;
+    return 0;
 }
 
 void matrix_print(Matrix* a) {
